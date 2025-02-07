ff0c2a332eabadee24a247ef8be47a78e8614a76
Arssh Bajpai <125165541+Arssh-Bajpai@users.noreply.github.com>
2025-02-06 20:31:47 -0500

    fin

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -22,13 +22,17 @@ export default function Dashboard() {
       <Row xs={1} md={4} className="g-4">
         {courses.map((course) => (
           <Col key={course.id} xs={12} sm={6} md={4} lg={3}>
-            <Card as={Link} to={`/Kambaz/Courses/${course.id}/Home`} className="shadow-sm text-decoration-none">
-              <Card.Img variant="top" src={course.img} style={{ width: "100%", height: "160px" }} />
-              <Card.Body>
-                <Card.Title className="text-dark">{course.title}</Card.Title>
-                <Card.Text className="text-muted">{course.desc}</Card.Text>
-              </Card.Body>
-            </Card>
+            <div>
+              <Link to={`/Kambaz/Courses/${course.id}/Home`} className="text-decoration-none">
+                <Card className="shadow-sm">
+                  <Card.Img variant="top" src={course.img} style={{ width: "100%", height: "160px" }} />
+                  <Card.Body>
+                    <Card.Title className="text-dark">{course.title}</Card.Title>
+                    <Card.Text className="text-muted">{course.desc}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </div>
           </Col>
         ))}
       </Row>
