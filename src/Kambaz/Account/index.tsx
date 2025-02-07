5346f591242de316d84ae0a35dfdbbb2f5084e0b
Arssh Bajpai <125165541+Arssh-Bajpai@users.noreply.github.com>
2025-02-06 19:06:56 -0500

    a2 CSS and Bootstrap

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import AccountNavigation from "../Navigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
-import Profile from "./Profile";
 
 export default function Account() {
   return (
     <div id="wd-account-screen" className="wd-account-container">
       <h2>Account</h2>
-
+      
       {/* Layout Wrapper */}
-      <div className="wd-account-layout d-flex">
+      <div className="wd-account-layout">
         {/* Left Sidebar (Navigation) */}
         <div className="wd-account-sidebar">
           <AccountNavigation />
@@ -19,13 +19,13 @@ export default function Account() {
         {/* Right Side (Content - Routes) */}
         <div className="wd-account-content">
           <Routes>
-            <Route path="/" element={<Navigate to="/Account/Signin" />} />
+            <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
             <Route path="/Signin" element={<Signin />} />
-            <Route path="/Signup" element={<Signup />} />
             <Route path="/Profile" element={<Profile />} />
+            <Route path="/Signup" element={<Signup />} />
           </Routes>
         </div>
       </div>
     </div>
   );
-}
+} 
