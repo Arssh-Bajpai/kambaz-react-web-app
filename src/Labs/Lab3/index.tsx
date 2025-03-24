42100ec6854f0c1c1710ba98f8550e81dff635b0
Arssh Bajpai <125165541+Arssh-Bajpai@users.noreply.github.com>
2025-03-24 03:19:34 -0400

    a4 labs

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { ListGroup, Button } from "react-bootstrap";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 import BooleanVariables from "./BooleanVariables";
@@ -19,65 +22,65 @@ import FindIndex from "./FindIndex";
 import FilterFunction from "./FilterFunction";
 import JsonStringify from "./JsonStringify";
 import House from "./House";
-import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
-import Spreading from "./Spreading";
-import Destructing from "./Destructing";
-import FunctionDestructing from "./FunctionDestructing";
-import DestructingImports from "./DestructingImports";
-import Classes from "./Classes";
-import Styles from "./Styles";
-import Add from "./Add";
-import Square from "./Square";
-import Highlight from "./Highlight";
-import AddPathParameters from "./AddPathParameters";
-import PathParameters from "./PathParameters";
 
 export default function Lab3() {
-  console.log('Hello World!');
-    return (
-      <div id="wd-lab3">
-        <h3>Lab 3</h3>
-        <VariablesAndConstants/>
-        <VariableTypes/>
-        <BooleanVariables/>
-        <IfElse/>
-        <TernaryOperator/>
-        <ConditionalOutputIfElse/>
-        <ConditionalOutputInline/>
-        <LegacyFunctions/>
-        <ArrowFunctions/>
-        <ImpliedReturn/>
-        <TemplateLiterals/>
-        <SimpleArrays/>
-        <ArrayIndexAndLength/>
-        <AddingAndRemovingToFromArrays/>
-        <ForLoops/>
-        <MapFunction/>
-        <FindFunction/>
-        <FindIndex/>
-        <FilterFunction/>
-        <JsonStringify/>
-        <House/>
-        <TodoItem/>
-        <TodoList/>
-        <Spreading/>
-        <Destructing/>
-        <FunctionDestructing/>
-        <DestructingImports/>
-        <Classes/>
-        <Styles/>
-        <Add a={3} b={4} />
-        <h4>Square of 4</h4>
-      <Square>4</Square>
+  console.log("Hello World!");
+
+  // ✅ State to track which section is active
+  const [activeSection, setActiveSection] = useState<"react" | "node">("react");
+
+  // ✅ Select 'todos' from Redux
+  const todos = useSelector((state: any) => state.todosReducer.todos);
+
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+
+      {/* ✅ Buttons to switch between sections */}
+      <Button variant={activeSection === "react" ? "primary" : "outline-primary"} onClick={() => setActiveSection("react")}>
+        Learn React
+      </Button>
+      <Button variant={activeSection === "node" ? "primary" : "outline-primary"} onClick={() => setActiveSection("node")}>
+        Learn Node
+      </Button>
+
       <hr />
-      <Highlight>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-     </Highlight>
-     <AddPathParameters/>
-     <PathParameters/>
-      </div>
-    );
-  }
-  
+
+      {activeSection === "react" && (
+        <ListGroup>
+          {todos.map((todo: any) => (
+            <ListGroup.Item key={todo.id}>{todo.title}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
+
+      {activeSection === "node" && (
+        <div>
+          <VariablesAndConstants />
+          <VariableTypes />
+          <BooleanVariables />
+          <IfElse />
+          <TernaryOperator />
+          <ConditionalOutputIfElse />
+          <ConditionalOutputInline />
+          <LegacyFunctions />
+          <ArrowFunctions />
+          <ImpliedReturn />
+          <TemplateLiterals />
+          <SimpleArrays />
+          <ArrayIndexAndLength />
+          <AddingAndRemovingToFromArrays />
+          <ForLoops />
+          <MapFunction />
+          <FindFunction />
+          <FindIndex />
+          <FilterFunction />
+          <JsonStringify />
+          <House />
+          <TodoList />
+        </div>
+      )}
+    </div>
+  );
+}
